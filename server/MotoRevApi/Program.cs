4149ac36b54774815cc5a1c48429e5b280566ae7
rdsalves <s.rodrigo5@pucpr.edu.br>
2026-06-03 18:42:08 -0300

    Update CORS policy, environment variable, and API base URL

--- server/MotoRevApi/Program.cs
+++ server/MotoRevApi/Program.cs
@@ -71,7 +71,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowReactApp", policy =>
     {
-        policy.WithOrigins("http://localhost:5173")
+        policy.WithOrigins("http://localhost:5173", "https://localhost:5173")
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
